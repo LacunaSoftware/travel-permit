20ff3650e1746c40bc25150ce9d32f650201ef72
Mateus Berardo de Souza Terra <mateusb@lacunasoftware.com>
2024-10-22 09:45:08 -0300

    Update hml key on web

--- angular/travelpermit-web/src/environments/environment.hml.ts
+++ angular/travelpermit-web/src/environments/environment.hml.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	cnbEndpoint: 'https://assinatura-hml.e-notariado.org.br',
 	ecKey: {
-		x: 'jYRkRf9yEeSKa2vT6B3QlaxRj23quRaXw/FBbuzPEQw',
-		y: 'iT6/b8wVsCcBd4ulqXlIUWkvY/8QscmK7vJS1wpPHls',
+		x: 'jYRkRf9yEeSKa2vT6B3QlaxRj23quRaXw_FBbuzPEQw',
+		y: 'iT6_b8wVsCcBd4ulqXlIUWkvY_8QscmK7vJS1wpPHls',
 	},
 };
